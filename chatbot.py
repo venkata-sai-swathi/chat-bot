3242441a42ce348d2f2382b51e4cb486fcf4c9f5
venkata-sai-swathi <61697502+venkata-sai-swathi@users.noreply.github.com>
2020-03-08 13:17:38 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,13 @@
-print("hello");
+rom ChatterBot.trainers import ListTrainer
+from ChatterBot import ChatBot
+
+bot=ChatBot('Test')
+conv=open('chats.txt','r').readlines()
+
+bot.set_trainer(ListTrainer)
+bot.train(conv)
+
+while True:
+    request=input('you :')
+    response=bot.get_response(request)
+    print('Bot :' , response)
